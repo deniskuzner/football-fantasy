3002adfc7e6566b928fe06ccccaa4d861139ba3c
programer20 <denis2607@hotmail.com>
2020-02-15 22:26:15 +0100

    Initial commit

---
+++ src/test/java/com/fon/footballfantasy/FootballFantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fon.footballfantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballFantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
