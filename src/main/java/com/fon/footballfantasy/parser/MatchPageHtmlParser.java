10cf8af14f2c08e4c9489db8826cc36cc832da72
deniskuzner <deniskuzner@gmail.com>
2020-04-05 16:42:27 +0200

    Dodato parsiranje odigranih minuta za svakog igraca na mecu. Refaktorisanje parsiranja meceva.

--- src/main/java/com/fon/footballfantasy/parser/MatchPageHtmlParser.java
+++ src/main/java/com/fon/footballfantasy/parser/MatchPageHtmlParser.java
@@ -14,29 +14,61 @@ import com.fon.footballfantasy.domain.Card;
 import com.fon.footballfantasy.domain.Club;
 import com.fon.footballfantasy.domain.Goal;
 import com.fon.footballfantasy.domain.MatchEvent;
+import com.fon.footballfantasy.domain.MinutesPlayed;
 import com.fon.footballfantasy.domain.Player;
 import com.fon.footballfantasy.domain.Substitution;
 
 @Component
 public class MatchPageHtmlParser {
-	
+
 	private static final String URL = "https://fbref.com/en/matches";
-	
+
 	public List<MatchEvent> parse(String matchUrl) throws IOException {
 
 		List<MatchEvent> result = new ArrayList<>();
-		
+
 		Document document = Jsoup.connect(URL + matchUrl).timeout(10000).get();
-		Elements clubs = document.getElementsByAttributeValue("itemprop", "performer");
 		
+		// Adding minutes played for host and guest players
+		Elements statsTables = document.select("table.stats_table tbody");
+		result.addAll(getMinutesPlayed(statsTables));
+		
+		Elements clubs = document.getElementsByAttributeValue("itemprop", "performer");
 		Club hostDTO = Club.builder().url(clubs.get(0).getElementsByAttributeValue("itemprop", "name").attr("href")).build();
 		Club guestDTO = Club.builder().url(clubs.get(1).getElementsByAttributeValue("itemprop", "name").attr("href")).build();
-		
-		Element table= document.getElementById("events_wrap");
-		Elements events = table.select("div.event");
 
+		// Adding goal, card and substitution events
+		Elements events= document.select("#events_wrap div.event");
+		result.addAll(getPlayByPlayEvents(events, hostDTO, guestDTO));
+		
+		return result;
+	}
+	
+	private List<MinutesPlayed> getMinutesPlayed(Elements statsTables) {
+		List<MinutesPlayed> result = new ArrayList<>();
+		Elements allPlayersRows = new Elements();
+		allPlayersRows.addAll(statsTables.get(0).select("tr"));
+		allPlayersRows.addAll(statsTables.get(2).select("tr"));
+		
+		for (Element hostPlayer : allPlayersRows) {
+			Player player = Player.builder().url(hostPlayer.select("th a").attr("href")).build();
+			int minutesPlayed;
+			try {
+				minutesPlayed = Integer.parseInt(hostPlayer.getElementsByAttributeValue("data-stat", "minutes").text());
+			} catch (NumberFormatException e) {
+				minutesPlayed = 0;
+			}
+			result.add(MinutesPlayed.builder().player(player)
+					.minutesPlayed(minutesPlayed).build());
+		}
+
+		return result;
+	}
+
+	private List<MatchEvent> getPlayByPlayEvents(Elements events, Club hostDTO, Club guestDTO) {
+		List<MatchEvent> result = new ArrayList<>();
+		
 		for (Element event : events) {
-			
 			MatchEvent matchEvent = null;
 			
 			String eventString = event.select("div").get(0).text();
@@ -76,12 +108,12 @@ public class MatchPageHtmlParser {
 		Player goalPlayer = Player.builder().url(event.select("a").attr("href")).build();
 		return Goal.builder().goalPlayer(goalPlayer).assistPlayer(null).build();
 	}
-	
+
 	private Card getCard(Element event, String card) {
 		Player player = Player.builder().url(event.select("a").attr("href")).build();
 		return Card.builder().card(card).player(player).build();
 	}
-	
+
 	private Substitution getSubstitution(Element event) {
 		Player inPlayer = Player.builder().url(event.select("a").get(0).attr("href")).build();
 		Player outPlayer = Player.builder().url(event.select("a").get(1).attr("href")).build();
